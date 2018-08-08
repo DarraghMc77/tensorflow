cefd425550cf5e846df532b0a2ac92ea72e8354a
DarraghMc77 <mccaffrd@tcd.ie>
2018-08-08 13:33:35 +0100

    added tiny-yolo graph to assets folder

--- tensorflow/examples/android/.externalNativeBuild/cmake/debug/armeabi-v7a/cmake_install.cmake
+++ tensorflow/examples/android/.externalNativeBuild/cmake/debug/armeabi-v7a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/Darragh/College/Dissertation/ThesisWriting/tensorflow/tensorflow/examples/android/jni
+# Install script for directory: /Users/Darragh/College/tensorflow/tensorflow/examples/android/jni
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/Darragh/College/Dissertation/ThesisWriting/tensorflow/tensorflow/examples/android/.externalNativeBuild/cmake/debug/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/Darragh/College/tensorflow/tensorflow/examples/android/.externalNativeBuild/cmake/debug/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
